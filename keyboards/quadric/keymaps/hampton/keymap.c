09867d71b4ed4c219247a84a22a49cadc09aeca4
Hampton Moore <me@hampton.pw>
2020-05-20 13:15:35 +0200

    Remove base layer arrow keys, align upper

--- keyboards/quadric/keymaps/hampton/keymap.c
+++ keyboards/quadric/keymaps/hampton/keymap.c
@@ -26,7 +26,7 @@ enum custom_keycodes {
 // Fillers to make layering more clear
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
-
+#define KC_PRT KC_PSCREEN
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|------+------+------+------+------+------|
      * |LShift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |      |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |      |LCtrl | LALT | LGUI |LOWER |SPACE |SPACE |Raise | Left | Down |  UP  |Right |
+     * |      |LCtrl | LALT | LGUI |LOWER |SPACE |SPACE |Raise |      |      |      |      |
      * `-----------------------------------------------------------------------------------'
      */
     [_QWERTY] = LAYOUT_ortho_5x12( \
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
             KC_GESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,  \
             KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX, \
-            XXXXXXX, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC, KC_SPC,   RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT  \
+            XXXXXXX, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC, KC_SPC,   RAISE,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX    \
             ),
 
     /* Dvorak
@@ -102,16 +102,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------------------------------'
      * |      | LEFT | DOWN |RIGHT |      |      |      |   -  |   =  |   [  |   ]  |  \   |
      * |------+------+------+------+------+------|------+------+------+------+------+------|
-     * |      |      |      |      |      |      |      |ISO # |ISO / |      |      |      |
+     * |      |      | Prnt |      |      |      |      |ISO # |ISO / |      |      |      |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
      * |      |      |      |      |      |      |      |      |      |      |      |      |
      * `-----------------------------------------------------------------------------------'
      */
     [_RAISE] = LAYOUT_ortho_5x12( \
-            KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-            _______, _______,   KC_UP, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-            _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-            _______, _______, _______, _______, _______, _______, _______, KC_NUHS, KC_NUBS, _______, _______, _______, \
+              KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
+            _______, XXXXXXX,   KC_UP, XXXXXXX, _______, _______, _______, _______, _______, _______, _______, _______, \
+            _______, KC_LEFT, KC_DOWN,KC_RIGHT, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
+            _______, _______,  KC_PRT, _______, _______, _______, _______, KC_NUHS, KC_NUBS, _______, _______, _______, \
             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
             ),
 
